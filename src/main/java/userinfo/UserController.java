3e6b2486fef5e5172809ec358b95837a9e656e84
Narahari Anuganti <nanuganti@ebsco.com>
2017-02-24 09:39:35 -0500

    Adding userinfo to hashmap and retrieving from same

--- src/main/java/userinfo/UserController.java
+++ src/main/java/userinfo/UserController.java
@@ -3,6 +3,7 @@ package userinfo;
 import java.util.HashMap;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -17,7 +19,7 @@ import io.swagger.annotations.ApiResponses;
 @Api("v1 - User Information ")
 public class UserController {
 	
-	HashMap<String, User> users = new HashMap<String, User>();
+	HashMap<Long, User> userinfo = new HashMap<Long, User>();
 	
 	@ApiOperation(value = "Get by ID.")
 	@ApiResponses(value = { 
@@ -28,14 +30,12 @@ public class UserController {
 			@ApiResponse(code = 404, message = "NOT_FOUND"),
 			@ApiResponse(code = 500, message = "INTERNAL SERVER ERROR") 
 		})
-	@RequestMapping(value="/GetUser",method = RequestMethod.GET )
-	public User GetUserInfo()
+	@RequestMapping(value="/GetUser/{id}",method = RequestMethod.GET )
+	public User GetUserInfo(
+			@PathVariable(name = "id", required = true) @ApiParam(value = "id nuber") long id)
 	{
-		User user1 = new User();
-		user1.setId(1);
-		user1.setFirstName("Nara");
-		user1.setLastName("Anuganti");
-		user1.setAge(25);
+		System.out.println("userId : "+id);
+		User user1 = userinfo.get(id);
 		return user1;
 	}
 	 
@@ -45,8 +45,8 @@ public class UserController {
 	{
 		 System.out.println("incoming user " + user.getLastName());
 		  //HashMap<String, User> users = new HashMap<String, User>();
-		  users.put(user.getLastName(), user);
-		  System.out.println(users);
-		  System.out.println(users.size());
+		 userinfo.put(user.getId(), user);
+		  System.out.println(userinfo);
+		  System.out.println(userinfo.size());
 	}
 }
